7dd8c29536c419ebc72082d640e187ca28c24890
Elvin <elvin_abdullayev1@mail.ru>
2018-10-11 10:34:55 +0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,76 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+//        int reqem=scanner.nextInt();
+//        System.out.print(menfidirmi(reqem));
+//    }
+//    static boolean menfidirmi(int reqem){
+//        return reqem<0;
+//    }
+
+
+
+//        int reqem=scanner.nextInt();
+//    System.out.print(fakt(reqem));
+//    }
+//    static int fakt(int reqem){
+//        int fak=1;
+//
+//        for(int i=1;i<=reqem;i++){
+//            fak=fak*i;
+//        }
+//        return fak;
+
+
+//        System.out.println("pattern daxil edin");
+//        int pattern=scanner.nextInt();
+//            System.out.println(funk(pattern));
+//
+//
+//    }
+//
+//  ***  static int funk(int pattern) {
+//        int dom
+//        for (int i = 1; i <= pattern; i++) {
+//
+//
+//            for (int a = 1; a <= pattern; a++) {
+//System.out.print(+a);
+//            }
+//System.out.print(" ");
+//
+//        }
+//       return
+
+
+
+
+
+//int a= scanner.nextInt();
+//int b=scanner.nextInt();
+//        System.out.print(hip(a,b));
+//    }
+//    static double hip(int a,int b){
+//
+//        int ckv;
+//                ckv=a*a+b*b;
+//                double hippo;
+//                hippo=Math.sqrt(ckv);
+//                return hippo;
+
+
+//    int a=scanner.nextInt();
+//            int b=scanner.nextInt();
+//            System.out.print(Mod(a,b));
+//    }
+//    static int Mod(int a,int b){
+//        int cek=a%b;
+//        return cek;
+    }
+}
+
+
